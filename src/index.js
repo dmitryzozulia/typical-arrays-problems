007b9a89fd85f30926f70727a305710f4e645ff8
dmitry_zozulia <z1dima1z@gmail.com>
2020-09-01 20:33:56 +0200

    resolve test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (min.arguments.length === 0) {
+        return 0
+    }
+    let minNumber = Infinity
+    for (let i = 0; i < array.length; i++) {
+        if (minNumber > array[i]) {
+            minNumber = array[i]
+        }
+    }
+    return minNumber === Infinity ? 0 : minNumber
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (max.arguments.length === 0) {
+        return 0
+    }
+    let maxNumber = -Infinity
+    for (let i = 0; i < array.length; i++) {
+        if (maxNumber < array[i]) {
+            maxNumber = array[i]
+        }
+    }
+    return maxNumber === -Infinity ? 0 : maxNumber
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let sum = 0
+    if (avg.arguments.length === 0) {
+        return 0
+    }
+    for (let i = 0; i < array.length; i++){
+        sum += array[i]
+    }
+    return isNaN(sum / array.length) ? 0 : sum / array.length
 }
